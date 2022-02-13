91f52f9a34c18b209d2fcb99eb2f43cd6c0f963f
prava-coder <96050882+prava-coder@users.noreply.github.com>
2022-02-13 20:01:56 +0530

    Add files via upload

---
+++ builtin_abs.py
@@ -0,0 +1,12 @@
+#for integer
+
+integer = -20
+print("The absolute value of given number is :",   abs(integer))
+
+# for float
+float_num = 45.6
+print("The absolute value of given number is:",abs(float_num))
+
+#for complex
+complex_num = 60+ 5j
+print("The absolute value of given num is :",abs(complex_num))
